541010c6790ae49131314a5a9be22ec443927e30
dennisolang <dennisolang@gmail.com>
2023-03-21 06:38:27 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/* print_alphabets in lower case a to z*/
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
