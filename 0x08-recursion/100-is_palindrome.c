0642070cd1a2916f6bde34ea0d70c930f9107088
dennisolang <dennisolang@gmail.com>
2023-04-05 01:35:22 -0700

    recursion is cool

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,18 @@
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to check
+ *
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+int len = strlen(s);
+for (int i = 0; i < len / 2; i++)
+{
+if (s[i] != s[len - i - 1])
+{
+return (0);
+}
+}
+return (1);
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+/**
+ * _strlen_recursion - Returns the length of a string
+ *
+ * @s: Pointer to the string to be measured
+ *
+ * Return: Integer representing the length of the string
+ */
+int _strlen_recursion(char *s);
+if (*s == '\0')
+return (0);
+return (1 + _strlen_recursion(s + 1));
+}
