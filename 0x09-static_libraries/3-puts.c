3a30807da52e7d34b90b3a34f475de560a84ff95
dennisolang <dennisolang@gmail.com>
2023-04-09 13:07:08 -0700

    static libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+void _puts(char *str)
+#include<stdio.h>
+/**
+ * main - Entry point
+ *_puts - output of characters and strings
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+_puts("\"Programming is proving to be fun\"\n\t- Dennis Olang");
+return (0);
+}
