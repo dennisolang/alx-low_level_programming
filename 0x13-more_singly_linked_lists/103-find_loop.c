f9c5143357f4c1083c05edd90d1ecefbdf856cd9
dennisolang <dennisolang@gmail.com>
2023-05-01 09:25:45 -0700

    singly_linked

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * find_listint_loop - finds the loop in a linked list
  * @head: linked list to search for
@@ -10,7 +9,7 @@ listint_t *find_listint_loop(listint_t *head)
 {
 listint_t *slow = head;
 listint_t *fast = head;
- if (!head)
+if (!head)
 return (NULL);
 while (slow && fast && fast->next)
 {
